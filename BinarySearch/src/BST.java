c915c9f60ce178cc6868ef98a630b475800fdda1
Ayue <98259022+Ayue9527@users.noreply.github.com>
2022-03-19 11:19:39 +0800

    第四次提交：     二叉搜索树，二分查找的延伸

--- BinarySearch/src/BST.java
+++ BinarySearch/src/BST.java
@@ -32,6 +32,36 @@ public class BST<E extends Comparable<E>> {
         return size == 0;
     }
 
+    // 向二分搜索树中添加新的元素e
+    public void add(E e) {
 
+        if (root == null) {
+            root = new Node(e);
+            size++;
+        } else {
+            add(root, e);
+        }
+    }
 
+    //向以node为根的二分搜索树中插入元素E，递归算法
+    private void add(Node node, E e) {
+        // 递归终点
+        if (e.equals(node.e)) {
+            return;
+        } else if (e.compareTo(node.e) < 0 && node.left == null) {
+            node.left = new Node(e);
+            size++;
+            return;
+        } else if (e.compareTo(node.e) > 0 && node.right == null) {
+            node.right = new Node(e);
+            size++;
+            return;
+        }
+
+        // 递归插入
+        if (e.compareTo(node.e) < 0)
+            add(node.left, e);
+        else    //e.compareTo(node.e)>0
+            add(node.right, e);
+    }
 }
