8a6cc2ef783d7a794d03ed0a47bc52c41ee01a68
Ayue9527 <98259022+Ayue9527@users.noreply.github.com>
2022-03-19 19:22:12 +0800

    第六次尝试提交

--- BinarySearch/src/BST.java
+++ BinarySearch/src/BST.java
@@ -2,6 +2,12 @@
     类型要满足拥有可比较性
  */
 
+import org.w3c.dom.Node;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class BST<E extends Comparable<E>> {
 
     private class Node {
@@ -74,5 +80,220 @@ public class BST<E extends Comparable<E>> {
             return contains(node.left, e);
         else
             return contains(node.right, e);
+    }
+
+    // 二分搜索树的前序遍历
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    // 前序遍历以node为根的二叉搜索树，递归
+    private void preOrder(Node node) {
+
+        if (node == null)
+            return;
+
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void preOrderNR() {
+
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            System.out.println(cur.e);
+
+            if (cur.right != null)
+                stack.push(cur.right);
+
+            if (cur.left != null)
+                stack.push(cur.left);
+        }
+    }
+
+    // 二分搜索树的中序遍历
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(Node node) {
+
+        if (node == null)
+            return;
+
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+
+    // 二分搜索树的后序遍历
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    private void postOrder(Node node) {
+
+        if (node == null)
+            return;
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.e);
+    }
+
+    // 二分搜索树的层序遍历（广度优先搜索）
+    public void levelOrder() {
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            Node cur = q.remove();
+            System.out.println(cur.e);
+
+            if (cur.left != null)
+                q.add(cur.left);
+            if (cur.right != null)
+                q.add(cur.right);
+        }
+    }
+
+    // 寻找二分搜索树的最小元素
+    public E minimum() {
+        if (size == 0)
+            throw new IllegalArgumentException("BST is empty!");
+
+        return minimum(root).e;
+    }
+
+    private Node minimum(Node node) {
+
+        if (node.left == null)
+            return node;
+
+        return minimum(node.left);
+    }
+
+    // 寻找二分搜索树的最大元素
+    public E maximum() {
+        if (size == 0)
+            throw new IllegalArgumentException("BST is empty!");
+
+        return maximum(root).e;
+    }
+
+    private Node maximum(Node node) {
+
+        if (node.right == null)
+            return node;
+
+        return maximum(node.right);
+    }
+
+    // 从二分搜索树中删除最小值所在节点，返回最小值
+    public E removeMin() {
+        E ret = minimum();
+        root = removeMin(root);
+        return ret;
+    }
+
+    // 删除掉以node为根的二分搜索树中的最小节点
+    // 返回删除节点后新的二分搜索树的根
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            Node rightNode = node.right;
+            node.right = null;
+            size--;
+            return rightNode;
+        }
+
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    // 从二分搜索树中删除最大值所在节点，返回最大值
+    public E removeMax() {
+        E ret = maximum();
+        root = removeMax(root);
+        return ret;
+    }
+
+    // 删除掉以node为根的二分搜索树中的最小节点
+    // 返回删除节点后新的二分搜索树的根
+    private Node removeMax(Node node) {
+        if (node.right == null) {
+            Node leftNode = node.left;
+            node.left = null;
+            size--;
+            return leftNode;
+        }
+
+        node.right = removeMax(node.right);
+        return node;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        generateBSTString(root, 0, res);
+        return res.toString();
+    }
+
+    private void generateBSTString(Node node, int depth, StringBuilder res) {
+        if (node == null) {
+            res.append(generateDepthString(depth)).append("null\n");
+            return;
+        }
+
+        res.append(generateDepthString(depth)).append(node.e).append("\n");
+        generateBSTString(node.left, depth + 1, res);
+        generateBSTString(node.right, depth + 1, res);
+    }
+
+    private String generateDepthString(int depth) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            res.append("--");
+        }
+
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5, 3, 6, 8, 4, 2};
+        for (int num : nums)
+            bst.add(num);
+
+        ////////////////////
+        //      5         //
+        //    /   \       //
+        //   3     6      //
+        //  / \     \     //
+        // 2   4     8    //
+        ////////////////////
+
+        bst.levelOrder();
+
+        //System.out.println(bst.getSize() + "," + bst.isEmpty());
+        //System.out.println();
+        //
+        //bst.preOrder();
+        //System.out.println();
+        //
+        //bst.preOrderNR();
+        //System.out.println();
+        //
+        //System.out.println(bst);
+        //
+        //bst.inOrder();
+        //System.out.println();
+        //
+        //bst.postOrder();
+        //System.out.println();
     }
 }
---
+++ BinarySearch/src/BSTRemoveTest.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class BSTRemoveTest {
+
+    public static void main(String[] args) {
+
+        BST<Integer> bst = new BST<>();
+        Random random = new Random();
+        int n = 1000;
+
+        for (int i = 0; i < n; i++) {
+            bst.add(random.nextInt(10000));
+        }
+
+        ArrayList<Integer> nums = new ArrayList<>();
+        // test removeMin
+        while (!bst.isEmpty())
+            nums.add(bst.removeMin());
+
+        System.out.println(nums);
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums.get(i - 1) > nums.get(i))
+                throw new IllegalArgumentException("Error");
+        }
+        System.out.println("removeMin test completed!");
+
+        // test removeMax
+        for (int i = 0; i < n; i++) {
+            bst.add(random.nextInt(10000));
+        }
+
+        nums = new ArrayList<>();
+        while (!bst.isEmpty())
+            nums.add(bst.removeMax());
+
+        System.out.println(nums);
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums.get(i - 1) < nums.get(i))
+                throw new IllegalArgumentException("Error");
+        }
+        System.out.println("removeMax test completed!");
+    }
+}
