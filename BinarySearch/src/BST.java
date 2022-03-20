391074b21e86918b2bd8ea136e27bf55b3d966d5
Ayue9527 <98259022+Ayue9527@users.noreply.github.com>
2022-03-20 21:27:43 +0800

    第七次提交     新增了Java类damnGit

--- BinarySearch/src/BST.java
+++ BinarySearch/src/BST.java
@@ -235,6 +235,56 @@ public class BST<E extends Comparable<E>> {
         return node;
     }
 
+    // 二分搜索树中删除任意节点
+    public void remove(E e) {
+        root = remove(root, e);
+    }
+
+    // 删除以node为根的二分搜索树中值为e的节点，递归
+    // 返回删除节点后新的二分搜索树的根
+    private Node remove(Node node, E e) {
+
+        if (node == null)
+            return null;
+
+        if (e.compareTo(node.e) < 0) {
+            node.left = remove(node.left, e);
+            return node;
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = remove(node.right, e);
+            return node;
+        } else {    // e == node.e
+
+            // 待删除节点左子树为空的情况
+            if (node.left == null) {
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                return rightNode;
+            }
+
+            // 待删除节点右子树为空的情况
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                return leftNode;
+            }
+
+            //  待删除节点左右子树都不为空的情况
+            //  找到比待删除节点大的最小节点，即待删除节点右子树的最小节点
+            //  用这个节点顶替待删除节点的位置
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);
+            //size++;
+            successor.left = node.left;
+
+            node.left = node.right = null;
+            //size--;
+            return successor;
+        }
+    }
+
     @Override
     public String toString() {
         StringBuilder res = new StringBuilder();
