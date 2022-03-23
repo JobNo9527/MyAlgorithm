af322a6fb196ddf54ef0993c4aef176efd79e4d0
Ayue9527 <846295837@qq.com>
2022-03-23 20:41:48 +0800

    第十次提交：     勾选了未上传的文件

---
+++ HeapSort/src/MaxHeap.java
@@ -0,0 +1,91 @@
+public class MaxHeap<E extends Comparable<E>> {
+
+    private Array<E> data;
+
+    public MaxHeap(int capacity) {
+        data = new Array<E>(capacity);
+    }
+
+    public MaxHeap() {
+        data = new Array<E>();
+    }
+
+    // 返回堆中的元素个数
+    public int size() {
+        return data.getSize();
+    }
+
+    // 返回一个布尔值，表示队长是否为空
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    // 返回完全二叉树的数组表示中，一个索引所表示的元素的父亲节点的索引
+    private int parent(int index) {
+        if (index == 0)
+            throw new IllegalArgumentException("index-0 doesn't have parent.");
+        return (index - 1) / 2;
+    }
+
+    // 返回完全二叉树的数组表示中，一个索引所表示的元素的左孩子节点的索引
+    private int leftChild(int index) {
+        return index * 2 + 1;
+    }
+
+    // 返回完全二叉树的数组表示中，一个索引所表示的元素的右孩子节点的索引
+    private int rightChild(int index) {
+        return index * 2 + 2;
+    }
+
+    // 向堆中添加元素
+    public void add(E e) {
+        data.addLast(e);    // 无需考虑容量，已实现自动扩容功能
+        siftUp(data.getSize() - 1);
+    }
+
+    private void siftUp(int k) {
+
+        while (k > 0 && data.get(parent(k)).compareTo(data.get(k)) < 0) {
+            data.swap(k, parent(k));
+            k = parent(k);  // 继续与现在位置的父亲节点大小比较
+        }
+    }
+
+    // 看堆中的最大元素
+    public E findMax() {
+        if (data.getSize() == 0)
+            throw new IllegalArgumentException("Can not findMax when heap is empty.");
+        return data.get(0);
+    }
+
+    // 取出堆中最大元素
+    public E extractMax() {
+
+        E ret = findMax();
+
+        data.swap(0, data.getSize() - 1);
+        data.removeLast();
+        siftDown(0);
+
+        return ret;
+    }
+
+    private void siftDown(int k) {
+
+        // 因为右孩子的索引比左孩子的索引还要大，如果左孩子的索引都越界了，右孩子的索引一定越界，也就是到了k是个叶子节点的时候了
+        while (leftChild(k) < data.getSize()) {
+
+            int j = leftChild(k);   // 此时j+1就是右孩子
+            if (j + 1 < data.getSize() && data.get(j + 1).compareTo(data.get(j)) > 0)
+                j = rightChild(k);  // j++;
+            // data[j] 是leftChild 和 rightChild中的最大值
+
+            // 下沉终止条件
+            if (data.get(k).compareTo(data.get(j)) >= 0)
+                break;
+
+            data.swap(k, j);
+            k = j;  // k下沉进行下一轮比较
+        }
+    }
+}
---
+++ HeapSort/src/SortingHelper.java
@@ -0,0 +1,35 @@
+/**
+ * @author 将晖
+ */
+
+
+public class SortingHelper {
+
+    private SortingHelper() {
+    }
+
+    //因为要实现比较，所以这个泛形需要实现一下comparable接口进行比较
+    public static <E extends Comparable<E>> boolean isSorted(E[] arr) {
+
+        for (int i = 1; i < arr.length; i++)
+            if (arr[i - 1].compareTo(arr[i]) > 0)   //如果能进去if说明不是有序的
+                return false;
+        return true;
+    }
+
+    public static <E extends Comparable<E>> void sortTest(String sortName, E[] arr,int n) {
+
+        //随机情况下
+        long start = System.nanoTime();
+        if (sortName.equals("HeapSort"))
+            HeapSort.sort(arr);
+        long end = System.nanoTime();
+        double time = (end - start) / 1000000000.0;
+
+        //验证排序是否正确
+        if (!SortingHelper.isSorted(arr))
+            throw new RuntimeException(sortName + " failed");
+
+        System.out.printf("%s , n = %d : %f s%n", sortName, arr.length, time);
+    }
+}
